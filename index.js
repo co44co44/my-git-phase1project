dc3b75e295f5c5406c3b1e77ba42bfe346d88047
Maria Cristina Ordonez <crisordonez44@gmail.com>
2022-11-08 13:35:41 -0500

    I am looking for dropdown working!

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function cardItem(item){
     card.className = 'card'
     card.innerHTML = `
 
-    <h4> "${item.name} @ ${item.store} @ ${item.mall}" </h4>
+    <h2> "${item.name} @ ${item.store} @ ${item.mall}" </h2>
 
     <img class ="item_picture" src = '${item.image}'>
     <p style="text-align:center">
@@ -40,20 +40,85 @@ function cardItem(item){
         </ul>
 
     `
-//3. add/append card to the dom inside the card
+//3. add/append card to the card area (DOM)
     document.querySelector('#sorted-collection').appendChild(card)
+    
    // likeBtnClick(item)
 }
 
 //4. initialize
+
+
+//3. filter the items by "I am looking for":
+//1. grab the "Im looking for" menu
+
+let dropdownLookingFor = document.querySelector('#looking-for-dropdown');
+let dropdownGender = document.querySelector('#breed-dropdown');
+
+
+//2. remove data displayed in the DOM by creating a function that tells what will happend 
+//when the dropdown is clicked
+
+function hideDropdownWhenClicked(){
+    dropdownLookingFor.addEventListener ('change', (e) =>{
+    removeAllChildNodesItems(document.querySelector('#sorted-collection'));
+    console.log ('3. items are hidding')
+    lookingForSelection()
+
+    document.querySelector('#sorted-collection').appendChild.innerHTML;
+    })}
+
+function removeAllChildNodesItems(parent){
+    while (parent.firstChild){
+        parent.removeChild(parent.firstChild);
+    }}
+
+// Code for the DROPDOWN "I am looking for":
+// create a function lookingForSelection() that: fetchs from server the data, filters it by a conditional:
+// if dropdown.value == to item.name, put the item in the cardItem function.
+// Then, upload it to the DOM with queryselector.appendChild
+
+function lookingForSelection(){
+     fetch('http://localhost:3000/items')
+    .then((response) => response.json())
+    .then((items) => items.filter(item => {
+        console.log (item.name)
+        //console.log (dropdownLookingFor.value)
+        if (item.name == dropdownLookingFor.value) {
+            console.log('4.fetch lookingfor dropdown ')
+            
+    cardItem(item)
+
+        document.querySelector('#sorted-collection').appendChild
+    }
+}))}; 
+ // CREATE THE GENDER DROPDOWN:
+//1. at the top (global scope) grab the gender dropdown with query selector 
+//2.create a function genderSelection() that: fetchs from server the data, 
+//filters it by a conditional: if dropdown.value == to item.name, put the item in the cardItem function.
+// Then, upload it to the DOM with queryselector.appendChild
+ function genderSelection(){
+    fetch('http://localhost:3000/items')
+   .then((response) => response.json())
+   .then((items) => items.filter(item => {
+       console.log (item.name)
+       //console.log (dropdownLookingFor.value)
+       if (item.name == dropdownLookingFor.value) {
+           console.log('4.fetch lookingfor dropdown ')
+           
+   cardItem(item)
+
+       document.querySelector('#sorted-collection').appendChild
+   }
+}))}; 
 function initialize(){
     fetchItems()
+    hideDropdownWhenClicked()
+    lookingForSelection()
 }
 initialize()
 
 
-//3. filter the items by "I am looking for":
-//1. grab the "Im looking for" menu 
 //2. filter items by user choice.
 
 
